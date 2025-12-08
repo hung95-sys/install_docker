9ee0af0c97285fae59a418f444c9baffe256d895
hung95-sys <vanbuuhung95@gmail.com>
2025-12-09 01:11:15 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
-# Add Docker's official GPG key:
+
+# Update hệ thống
 sudo apt-get update -y
-sudo apt-get install ca-certificates curl -y
+sudo apt-get install -y ca-certificates curl
+
+# Tạo thư mục key nếu chưa có
 sudo install -m 0755 -d /etc/apt/keyrings
+
+# Tải Docker GPG key
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# Thêm Docker repository
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update lại repo
 sudo apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# Cài Docker CE & Compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-sudo dpkg --configure -a
-sudo apt install containerd.io=1.7.28-1~ubuntu.24.04~noble -y
+# Khởi động lại dịch vụ
 sudo systemctl restart containerd
 sudo systemctl restart docker
 
+echo "---- Docker installation completed ----"
+docker --version
+docker compose version
