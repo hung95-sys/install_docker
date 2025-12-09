cb0fd825554fd10ae10c55f9840596463b4212f2
hung95-sys <vanbuuhung95@gmail.com>
2025-12-09 08:26:20 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -18,8 +18,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-sudo dpkg --configure -a
-sudo apt install containerd.io=1.7.28-1~ubuntu.24.04~noble -y
+sudo apt install containerd.io=1.7.28-1~ubuntu.24.04~noble -y --allow-downgrades
 sudo systemctl restart containerd
 sudo systemctl restart docker
+
